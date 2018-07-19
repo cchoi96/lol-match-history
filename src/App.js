46ddbdc258a237c4df4b412f5e41e5dce35e6910
Chris Choi <chrischoi96@gmail.com>
2018-07-19 12:36:12 -0400

    Add Header component and some general styling

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header.js';
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = { message: '' };
-  }
-
-  componentDidMount() {
-    fetch('/api/message')
-      .then(response => response.json())
-      .then(json => this.setState({ message: json }));
-  }
 
   render() {
     return (
       <div className="App">
+      
       </div>
     );
   }
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './Header.css';
+
+const header = () => {
+        return (
+            <div className='header'>
+                <h1>LEAGUE OF LEGENDS</h1>
+                <h2 className='red'>MATCH HISTORY</h2>
+            </div>
+        );
+}
+
+export default header;
